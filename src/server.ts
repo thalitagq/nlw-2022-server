ea552c5c81ecbbcc50b710338bec96b0effe4ac9
thalita <thalitagq@outlook.com>
2022-05-09 17:03:54 -0300

    cors error

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,12 @@ const app = express()
 const cors = require("cors");
 
 app.use(cors());
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.json())
 app.use(routes);
 

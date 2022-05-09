25ec46bd7e3768e3b8cbd1006f21d521db1e8447
thalita <thalitagq@outlook.com>
2022-05-09 18:12:13 -0300

    cors error

--- src/server.ts
+++ src/server.ts
@@ -5,19 +5,17 @@ import { routes } from './routes'
 const app = express()
 const cors = require("cors");
 
-// app.use(cors());
-// app.all("*", function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   res.header("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// });
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors());
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 
 app.use(express.json())
 app.use(routes);
 
-app.listen(process.env.PORT || 3333, ()=> {
+app.listen(process.env.PORT, ()=> {
   console.log("HTTP server running")
 })
